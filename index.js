dccd5b5b54bb0e062fb1501233278e9694cbe226
Marc Droger <marcdroger@hotmail.com>
2022-04-27 21:47:16 +0200

    Moved css directory to public & added template pug to other pug files

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ const port = 3000;
 //use static public directory
 app.use(express.static('public'))
 
-//use css directory
-app.use(express.static('css'))
-
 //set templating engine to pug
 app.set('view engine', 'pug')
 
