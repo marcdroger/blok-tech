b5f0bc78e93d50988f11402180c6c08dd2355c67
Marc Droger <marcdroger@hotmail.com>
2022-04-27 20:43:03 +0200

    Added favicon

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//use static public directory
 app.use(express.static('public'))
+
+//set templating engine to pug
 app.set('view engine', 'pug')
 
 //render index page
