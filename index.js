eecfba67250afa67d6c947eef227f93d44588b9c
Marc Droger <marcdroger@hotmail.com>
2022-04-27 20:05:16 +0200

    Added 404 page

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('hello world')
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'))
+app.set('view engine', 'pug')
+
+app.get('/', (res) => {
+  res.render('index');
+})
+
+app.use((res) => {
+  res.status(404).render('404');
+})
+
+app.listen(port, () => {
+  console.log(`express running on port ${port}`);
+})
