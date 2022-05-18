f343b84714e55597bb0a5c22106c476173ec72af
Marc Droger <marcdroger@hotmail.com>
2022-05-18 14:52:26 +0200

    Add form reads student data.

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const app = express();
 
+const multer = require('multer');
+const upload = multer({ dest: 'public/upload'});
+
 const port = 3000;
 
-require('dotenv').config()
+require('dotenv').config();
 
 //use static public directory
 app.use(express.static('public'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//TODO: add validator
+
+app.post('/account', upload.single('avatar'), (req, res) => {
+  console.log(req.body)
+
+  //TODO: add account update succes or error messages
+  //res.redirect('back');
+})
 
 //set templating engine to pug
 app.set('view engine', 'pug');
