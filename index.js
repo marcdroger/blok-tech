f799a74801a23c7b1253bf1e6f9e01133fdc5a5a
Marc Droger <marcdroger@hotmail.com>
2022-05-30 02:40:47 +0200

    Get results from database. Read results in pug.

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
 const express = require('express');
-const { MongoClient, ObjectId } = require('mongodb');
 const app = express();
-
-//require the dotenv package for dotenv variables
-require('dotenv').config();
-
-//environment variables
-const port = process.env.DB_PORT || 3000;
-const uri = process.env.DB_URI;
-const db = process.env.DB_DATABASE;
-const collection = process.env.DB_COLLECTION;
-const mapboxAPI = process.env.MAPBOX_KEY;
-
+const { MongoClient, ObjectId } = require('mongodb');
 
 //use static public directory
 app.use(express.static('public'));
@@ -20,6 +9,36 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//require the dotenv package for dotenv variables
+require('dotenv').config();
+
+//environment variables
+const port = process.env.DB_PORT || 3000;
+const url = process.env.DB_URI;
+const databaseName = process.env.DB_DATABASE;
+const collection = process.env.DB_COLLECTION;
+const mapboxAPI = process.env.MAPBOX_KEY;
+
+let db;
+
+//set templating engine to pug
+app.set('view engine', 'pug');
+
+//render index page
+app.get('/', async (req, res) => {
+  const students = await db.collection(collection).find({},{}).toArray();
+
+  res.render('index', {
+    mapboxAPI, students
+  });
+})
+
+
+
+//render account page
+app.get('/account', (req, res) => {
+  res.render('account');
+})
 
 //TODO: add validator data later?
 app.post('/account', (req, res) => {
@@ -29,49 +48,30 @@ app.post('/account', (req, res) => {
   //res.redirect('back');
 })
 
+//render 404 page
+app.use((req, res) => {
+  res.status(404).render('404');
+})
+
 //connect to MongoDB database
-async function connectMongo() {
-  const client = new MongoClient(uri, {
+async function connectDB() {
+  const client = new MongoClient(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
   try {
     await client.connect();
-    client.db(process.env.DB_DATABASE);
+    db = client.db(databaseName);
   } catch (error) {
     throw error;
   }
 }
-
-//set templating engine to pug
-app.set('view engine', 'pug');
-
-//render index page
-app.get('/', async (req, res) => {
-  //const students = await db.collection(process.env.DB_COLLECTION).find({},{}).toArray();
-
-  //console.log(students)
-
-  res.render('index', {
-    mapboxAPI
-  });
-})
-
-//render account page
-app.get('/account', (req, res) => {
-  res.render('account');
-})
-
-//render 404 page
-app.use((req, res) => {
-  res.status(404).render('404');
-})
 
 //listen on port for changes
 app.listen(port, () => {
   console.log(`express running on port ${port}`);
 
   //check if mongoDB connection succes
-  //connectMongo().then(console.log('Connected to MongoDB'));
+  connectDB().then(console.log('Connected to MongoDB'));
 })
 
